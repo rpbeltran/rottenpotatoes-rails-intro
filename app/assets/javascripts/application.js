c190d7e5cfde6753a78b2acb97bd0e6ad2c51a6b
rpbeltran <rp.beltran@yahoo.com>
2019-09-26 13:32:46 -0500

    trigger hilite on Onload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,12 +27,12 @@ function getSortby () {
 }
 
 
-document.onload( function () {
-	var sb = getSortby();
+document.addEventListener("DOMContentLoaded", function(){
+  var sb = getSortby();
 
 	if ( sb ) {
 		
 		document.getElementById( sb ).className += " hilite";
 
 	}
-} )
+});
