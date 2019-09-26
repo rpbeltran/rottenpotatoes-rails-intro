470f392aeef3665014f61938f9d74f0fa1791206
rpbeltran <rp.beltran@yahoo.com>
2019-09-26 13:47:25 -0500

    trigger hilite on Onload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,8 +38,18 @@ function hilite () {
 	}
 }
 
-document.addEventListener("DOMContentLoaded", hilite );
-document.onload = hilite;
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
+
+ready( hilite );
 
 
 
